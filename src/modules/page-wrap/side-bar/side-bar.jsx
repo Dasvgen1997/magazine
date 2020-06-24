484433cda95e01238c6906f715817e10af70919c
Dasvgen1997 <dasvgen@gmail.com>
2020-06-24 12:47:16 +0300

    update author

--- src/modules/page-wrap/side-bar/side-bar.jsx
+++ src/modules/page-wrap/side-bar/side-bar.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './side-bar.sass';
 import classnames from 'classnames';
 
-import logoImg from './../../../files/img/logo.png'
+import logoImg from './../../../files/img/logo.png';
 
-export default function SideBar({className}){
-    return(
-        <div className={classnames('side-bar', className)}>
-            <form className='side-bar__subscribe-form'>
-                <img src={logoImg} />
-            </form>
-        </div>
-    )
+import TagList from './../../../components/tag-list/tag-list.jsx';
+
+function BlockWrap({ className, children, title }) {
+	return (
+		<div className={classnames('side-bar__block-wrap', className)}>
+			<div className="side-bar__block-wrap-title">
+				<span className="icon-right-dir" />
+				<span>{title}</span>
+			</div>
+
+			{children}
+		</div>
+	);
+}
+
+export default function SideBar({ className }) {
+	let [ email, setEmail ] = useState('');
+
+	function subscribeHandler(e) {
+		e.preventDefault();
+	}
+
+	return (
+		<div className={classnames('side-bar', className)}>
+			<form onSubmit={subscribeHandler} className="side-bar__subscribe-form">
+				<img src={logoImg} />
+				<div className="side-bar__field-wrap">
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="Введите ваш email..."
+					/>
+				</div>
+				<button className="side-bar__subscribe-submit" type="submit">
+					Подписаться
+				</button>
+			</form>
+
+			<BlockWrap className='side-bar__tag-cloud' title="Теги">
+				<TagList />
+			</BlockWrap>
+
+            <BlockWrap className='side-bar__latest' title="Последние">
+				йцвйцв
+			</BlockWrap>
+		</div>
+	);
 }
--- src/pages/author/author.jsx
+++ src/pages/author/author.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './author.sass';
 
 import PageWrap from './../../modules/page-wrap/page-wrap.jsx';
+import AuthorCard from './../../components/author-card/author-card.jsx';
 
 export default function Author(){
     return(
         <div className='author-page'>
             <PageWrap title='Jonas Khanwald'>
-                sda
+                <AuthorCard/>
             </PageWrap>
-            qwd
         </div>
     )
 }
